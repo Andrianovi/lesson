f8794d9e328cb0e7a00125919a714599c9d89561
Andrianovi <106331579+Andrianovi@users.noreply.github.com>
2023-10-22 01:30:25 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,479 @@
+
+//@prepros-append jq-start.js
+//@prepros-append responsive.js
+//@prepros-append map.js
+//@prepros-append forms.js
+//@prepros-append script.js
+//@prepros-append jq-end.js
+
+
+
+const form = document.forms[0];
+console.log(form);
+form.addEventListener('focusin', function (event) {
+	console.log('фокус');
+	const pls = event.target.closest('.input').getAttribute('placeholder');
+	event.target.closest('.input').setAttribute('placeholder', '');
+	event.target.closest('.input').addEventListener('focusout', function (event) {
+		event.target.closest('.input').setAttribute('placeholder', pls);
+	});
+	event.target.closest('.input').classList.remove('err');
+});
+
+//console.log(form.closest('.input')[0].value.length);
+
+form.addEventListener('submit', function (event) {
+	event.preventDefault();
+	const input = form.querySelectorAll('.input');
+	let error = 0;
+	input.forEach(function (block, item, array) {
+		if (block.value.length < 1) {
+			block.classList.add('err');
+			error = error + 1
+		}
+	});
+
+	if (emailTest(input[1]) && Boolean(Number(input[1].value) / 1) !== true) {
+		input[1].classList.add('err');
+		error = error + 1
+	}
+
+	if (error === 0) {
+		form.submit();
+	}
+});
+
+function emailTest(input) {
+	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+/// Функция теста email
+
+
+
+
+
+//*@ При ширине окна браузера менее 767 добавляет, к <div class="header-contacts">, класс 'done', или убирает его если ширина окна больше и при любом раскладе засовывает этот блок в <div class='header-menu'>
+
+//* событие 'resize' - срабатывает, только с window, на изменение оzкна браузера и не прекращает своего действия до перезагрузки
+
+$(window).resize(function (event) {
+	adaptive_function();
+});
+function adaptive_header(w, h) {
+	var headerMenu = $('.header-menu');
+	var headerLang = $('.header-top-lang');
+	if (w < 767) {
+		if (!headerLang.hasClass('done')) {
+			headerLang.addClass('done').appendTo(headerMenu);
+
+		}
+	} else {
+		if (headerLang.hasClass('done')) {
+			headerLang.removeClass('done').prependTo($('.header-top'));
+		}
+	}
+	//* При ширине меньше 767 кнопки языков переезжают в начало блока .header-top и убирает клас 'done', а если ширина больше, кнопки языков на месте, с добавлением класса "done"
+
+	if (w < 767) {
+		if (!$('.header-bottom-menu').hasClass('done')) {
+			$('.header-bottom-menu').addClass('done').appendTo(headerMenu);
+
+		}
+	} else {
+		$.each($('.header-bottom-menu'), function (index, val) {
+			if ($(this).hasClass('header-bottom-menu--right')) {
+				if ($(this).hasClass('done')) {
+					$(this).removeClass('done').prependTo($('.header-bottom__column').eq(2));
+					// закидывает .header-bottom-menu, в .header-bottom__column, c индексом 2, то есть третьим с начала
+				}
+			} else {
+				if ($(this).hasClass('done')) {
+					$(this).removeClass('done').prependTo($('.header-bottom__column').eq(0));
+					// закидывает .header-bottom-menu, в .header-bottom__column, c индексом 0, то есть первым с начала
+				}
+			}
+		});
+	}
+}
+
+
+function adaptive_function() {
+	var w = $(window).outerWidth();
+	var h = $(window).outerHeight();
+	adaptive_header(w, h);
+}
+adaptive_function();
+//console.log($('.header-bottom-menu'));
+function map(n) {
+	google.maps.Map.prototype.setCenterWithOffset = function (latlng, offsetX, offsetY) {
+		var map = this;
+		var ov = new google.maps.OverlayView();
+		ov.onAdd = function () {
+			var proj = this.getProjection();
+			var aPoint = proj.fromLatlngToContainerPixel(latlng);
+			aPoint.x = aPoint.x + offsetX;
+			aPoint.y = aPoint.y + offsetY;
+			map.panTo(proj.fromContainerPixelToLatLng(aPoint));
+			//map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
+		}
+		ov.draw = function () { };
+		ov.setMap(this);
+	};
+	var markers = new Array();
+	var infowindow = new google.maps.InfoWindow({
+		//pixelOffset: new google.maps.Size(-230,250)
+	});
+	var locations = [
+		[new google.maps.LatLng(53.819055, 27.8813694)]
+	]
+	var options = {
+		zoom: 10,
+		panControl: false,
+		mapTypeControl: false,
+		center: locations[0][0],
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById('map'), options);
+	var icon = {
+		url: 'img/icons/map.svg',
+		scaledSize: new google.maps.Size(18, 20),
+		anchor: new google.maps.Point(9, 10)
+	}
+	for (var i = 0; i < locations.length; i++) {
+		var marker = new google.maps.Marker({
+			icon: icon,
+			//position: locations[1][0],
+			map: map,
+		});
+		markers.push(marker);
+	}
+}
+if ($('#map').length > 0) {
+	map();
+}
+//* В свой файлик main.js, подключаю отдельный файлик map.js, где у меня есть заготовочка для карты. Что происходит в файлике map.js:
+//* 1) Фактически подключается карта к блоку с id="map"(стр.31); 
+/* //FORMS
+function forms() {
+	//SELECT
+	if ($('select').length > 0) {
+		function selectscrolloptions() {
+			var scs = 100;
+			var mss = 50;
+			if (isMobile.any()) {
+				scs = 10;
+				mss = 1;
+			}
+			var opt = {
+				cursorcolor: "#2078e5",
+				cursorwidth: "4px",
+				background: "",
+				autohidemode: false,
+				bouncescroll: false,
+				cursorborderradius: "0px",
+				scrollspeed: scs,
+				mousescrollstep: mss,
+				directionlockdeadzone: 0,
+				curorborder: "0px solid #fff",
+			};
+			return opt;
+		}
+
+		function select() {
+			$.each($('select'), function (index, val) {
+				var ind = index;
+				$(this).hide();
+				if ($(this).parent('.select-block').length == 0) {
+					$(this).wrap("<div class='select-block " + $(this).attr('class') + "-select-block'></div>");
+				} else {
+					$(this).parent('.select-block').find('.select').remove();
+				}
+				var milti = '';
+				var check = '';
+				var sblock = $(this).parent('.select-block');
+				var soptions = "<div class='select-options'><div class='select-options-scroll'><div class='select-options-list'>"
+				if ($(this).attr('multiple') == 'multiple') {
+					milti = 'multiple';
+					check = 'check';
+				}
+				$.each($(this).find('option'), function (index, val) {
+					if ($(this).attr('value') != '') {
+						soptions = soptions + "<div data-value='" + $(this).attr('value') + "' class='select-options__value_" + ind + " select-options__value value_" + ind + "'></div>";
+					} else if ($(this).parent().attr('data-label') == 'on') {
+						if (sblock.find('.select__label').length == 0) {
+							sblock.prepend('<div class="select__label">' + $(this).html() + '</div>');
+						}
+					}
+				});
+				soptions = soptions + "</div></div></div>";
+				if ($(this).attr('data-type') == 'search') {
+					sblock.append("<div data-type='search' class='select_" + ind + " select" + " " + $(this).attr('class') + "__select " + milti + "'>" +
+						"<div class='select-title'>" +
+						"<div class='select-title_arrow ion-ios-arrow-down'></div>" +
+						"<input data-value='" + $(this).find('option[selected="selected"]').html() + "' class ='select-title__value value__" + ind + "' />" +
+						"</div>" +
+						soptions +
+						"</div>");
+					$('.select_' + ind).find('input.select-title__value').jcOnPageFilter({
+						parentSectionClass: 'select-options_' + ind,
+						parentLookupClass: 'select-options__value_' + ind,
+						childBlockClass: 'select-options__value_' + ind
+					});
+				} else {
+					sblock.append("<div class='select_" + ind + " select" + " " + $(this).attr('class') + "__select " + milti + "'>" +
+						"<div class='select-title'>" +
+						"<div class='select-title__arrow ion-ios-arrow-down'></div>" +
+						"<div class='select-title__value value_" + $(this).find('option[selected="selected"]').val() + "'>" +
+						"</div>" +
+						soptions +
+						"</div>");
+				}
+				if ($(this).find('option[selected="selected"]').val() != '') {
+					sblock.find('.select').addClass('focus');
+				}
+				if ($(this).attr('data-req') == 'on') {
+					$(this).addClass('req');
+				}
+				$(".select_" + ind + " .select-options-scroll").niceScroll('.select-options-list', selectscrolloptions());
+			});
+		}
+		select();
+
+		$('body').on('keyup', 'input.select-title__value', function () {
+			$('.select').not($(this).parents('.select')).removeClass('active').find('.select-options').slideUp(50);
+			$(this).parents('.select').addClass('active');
+			$(this).parents('.select').find('.select-options').slideDown(50, function () {
+				$(this).find(".select-options-scroll").getNiceScroll().resize();
+			});
+			$(this).parents('.select-block').find('select').val('');
+		});
+		$('body').on('click', '.select', function () {
+			if (!$(this).hasClass('disabled')) {
+				$('.select').not(this).removeClass('active').find('.select-options').slideUp(50);
+				$(this).toggleClass('active');
+				$(this).find('.select-options').slideToggle(50, function () {
+					$(this).find(".select-options-scroll").getNiceScroll().resize();
+				});
+
+				//
+			}
+		});
+	}
+} */
+var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); } };
+//if (isMobile.any()) { }
+
+if (location.hash) {
+	var hsh = location.hash.replace('#', '');
+	if ($('.popup-' + hsh).length > 0) {
+		popupOpen(hsh);
+	} else if ($('div.' + hsh).length > 0) {
+		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
+	}
+}
+$('.wrapper').addClass('loaded');
+
+var act = "click";
+/* if (isMobile.iOS()) {
+	var act = "touchstart";
+} */
+$('.header-menu__icon').click(function (event) {
+	$(this).toggleClass('active');
+	$('.header-menu').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+//*@ Сия заготовка гласит о том, что при нажатии на .header-menu__icon происходят некоторые действия
+
+
+//* ZOOM
+if ($('.gallery').length > 0) {
+	baguetteBox.run('.gallery', {
+		//Custom options
+	});
+}
+
+//* POPUP
+$('.p1').click(function (event) {
+	var p1 = $(this).attr('href').replace('#', '');
+	var v = $(this).data('vid');
+	popupOpen(p1, v);
+	return false;
+});
+function popupOpen(p1, v) {
+	$('.popup').removeClass('active').hide();
+	if (!$('.header-menu').hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	if (!isMobile.any()) {
+		$('body').css({ paddingRight: $(window).outerWidth() - $('.wrapper').outerWidth() }).addClass('lock');
+		$('.pdb').css({ paddingRight: $(window).outerWidth() - $('.wrapper').outerWidth() });
+	} else {
+		setTimeout(function () {
+			$('body').addClass('lock');
+		}, 300);
+	}
+	history.pushState('', '', '#' + p1);
+	if (v != '' && v != null) {
+		$('.popup-' + p1 + ' .popup-video__value').html('<iframe src="https://www.youtube.come/embed/' + v + '?autoplay=1" allow="autoplay; encrypted-media" ></iframe>');
+	}
+	$('.popup-' + p1).fadeIn(300).delay(300).addClass('active');
+
+	if ($('.popup-' + p1).find('.slick-slider').length > 0) {
+		$('.popup-' + p1).find('.slick-slider').slick('setPosition');
+	}
+}
+function openPopupById(popup_id) {
+	$('#' + popup_id).fadeIn(300).delay(300).addClass('active');
+}
+function popupClose() {
+	$('.popup').removeClass('active').fadeOut(300);
+	if (!$('.header-menu').hasClass('active')) {
+		if (!isMobile.any()) {
+			setTimeout(function () {
+				$('body').css({ paddingRight: 0 });
+				$('.pdb').css({ paddingRight: 0 });
+			}, 200);
+			setTimeout(function () {
+				$('body').removeClass('lock');
+				$('body,html').scrollTop(parseInt($('body').data('scroll')));
+			}, 200);
+		} else {
+			$('body').removeClass('lock');
+			$('body,html').scrollTop(parseInt($('body').data('scroll')));
+		}
+	}
+	$('.popup-video__value').html('');
+
+	history.pushState('', '', window.location.href.split('#')[0]);
+}
+$('.popup-close,.popup__close').click(function (event) {
+	popupClose();
+	return false;
+});
+$('.popup').click(function (e) {
+	if (!$(e.target).is(".popup>.popup-table>.cell *") || $(e.target).is(".popup-close") || $(e.target).is(".popup__close")) {
+		popupClose();
+		return false;
+	}
+});
+$(document).on('keydown', function (e) {
+	if (e.which == 27) {
+		popupClose();
+	}
+});
+$('.goto').click(function () {
+	var el = $(this).attr('href').replace('#', '');
+	var offset = 0;
+	$('body,html').animate({ scrollTop: $('.' + el).offset().top + offset }, 500, function () { });
+
+	if ($('.header-menu').hasClass('active')) {
+		$('.header-menu,.header-menu__icon').removeClass('active');
+		$('body').removeClass('lock');
+	}
+	return false;
+});
+//*Делает картинку с классом img - фоном, но при этом можно не лезть в CSS, каждый раз при смене <img>
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		//if ($(this).find('img').length > 0) {
+		$(this).css('background', 'url("' + $(this).find('img').attr('src') + '") 50% / cover no-repeat');
+		this.querySelector('img').setAttribute('hidden', 'true');
+	});
+}
+ibg();
+
+//Клик вне области
+$(document).on('click touchstart', function (e) {
+	if (!$(e.target).is(".select *")) {
+		$('.select').removeClass('active');
+	};
+});
+
+//UP
+$(window).scroll(function () {
+	var w = $(window).width();
+	if ($(window).scrollTop() > 50) {
+		$('#up').fadeIn(300);
+	} else {
+		$('#up').fadeOut(300);
+	}
+});
+$('#up').click(function (event) {
+	$('body, html').animate({ scrollTop: 0 }, 300);
+});
+
+$('body').on('click', '.tab__navitem', function (event) {
+	var eq = $(this).index();
+	if ($(this).hasClass('parent')) {
+		var eq = $(this).parent().index();
+	}
+	if (!$(this).hasClass('active')) {
+		$(this).closest('.tabs').find('.tab__navitem').removeClass('active');
+		$(this).addClass('active');
+		$(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+		if ($(this).closest('.tabs').find('.slick-slider').length > 0) {
+			$(this).closest('.tabs').find('.slick-slider').slick('setPosition');
+		}
+	}
+});
+$.each($('.spoller.active'), function (index, val) {
+	$(this).next().show();
+});
+$('body').on('click', '.spoiler', function (event) {
+	if ($(this).hasClass('mob') && !isMobile.any()) {
+		return false;
+	}
+	if ($(this).hasClass('closeall') && !$(this).hasClass('active')) {
+		$.each($(this).closest('.spoiler').find('.spoiler'), function (index, val) {
+			$(this).removeClass('active');
+			$(this).next().slideUp(300);
+		});
+	}
+	$(this).toggleClass('active').next().slideToggle(300, function (index, val) {
+		if ($(this).parent().find('.slick-slider').length > 0) {
+			$(this).parent().find('.slick-slider').slick('setPosition');
+		}
+	});
+	return false;
+});
+
+
+function scrolloptions() {
+	var scs = 100;
+	var mss = 50;
+	var bns = false;
+	if (isMobile.any()) {
+		scs = 10;
+		mss = 1;
+		bns = true;
+	}
+	var opt = {
+		cursorcolor: "#fff",
+		cursorwidth: "4px",
+		background: "",
+		autohidemode: true,
+		cursoropacitymax: 0.4,
+		bouncescroll: bns,
+		cursorborderradius: "0px",
+		scrollspeed: scs,
+		mousescrollstep: mss,
+		directionlockdeadzone: 0,
+		curorborder: "0px solid #fff",
+	};
+	return opt;
+}
+function scroll() {
+	$('.scroll-body').niceScroll('.scroll-list', scrolloptions());
+}
+if (navigator.appVersion.indexOf("Mac") != -1) {
+} else {
+	if ($('.scroll-body').length > 0) { scroll(); }
+}
+
